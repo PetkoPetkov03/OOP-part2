e1bb5c0f1c80ce3bf0f5337fb3ef067f5d00c794
PetkoPetkov03 <komandir1612@gmail.com>
2024-10-04 21:30:29 +0300

    Initial commit template

---
+++ src/main/java/com/sparks/of/fabrication/oop2/Application.java
@@ -0,0 +1,24 @@
+package com.sparks.of.fabrication.oop2;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("build.css")).toExternalForm());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
