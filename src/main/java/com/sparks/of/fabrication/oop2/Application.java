18407f1d058d86f479781283bc11f031e680c4d1
PetkoPetkov03 <komandir1612@gmail.com>
2024-10-08 10:20:51 +0300

    Setting up JPA Wrapper, Logger and fixing persistence.xml

--- src/main/java/com/sparks/of/fabrication/oop2/Application.java
+++ src/main/java/com/sparks/of/fabrication/oop2/Application.java
@@ -1,13 +1,21 @@
 package com.sparks.of.fabrication.oop2;
 
+import com.sparks.of.fabrication.oop2.utils.EntityManagerWrapper;
+import com.sparks.of.fabrication.oop2.utils.Env;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class Application extends javafx.application.Application {
+    private static final Env env = new Env();
+    private static final EntityManagerWrapper entityManagerWrapper = new EntityManagerWrapper(env);
+    private static final Logger log = LogManager.getLogger(Application.class);
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
@@ -18,7 +26,21 @@ public class Application extends javafx.application.Application {
         stage.show();
     }
 
+    private static void exit() {
+        boolean exit = entityManagerWrapper.cleanUp();
+
+        if(exit) {
+            log.info("Application exited successfully");
+            System.exit(0);
+        }
+        else {
+            log.info("Application exited unsuccessfully");
+            System.exit(1);
+        }
+    }
+
     public static void main(String[] args) {
         launch();
+        exit();
     }
 }
